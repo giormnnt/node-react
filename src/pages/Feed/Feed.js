7c7be69bc50aba5abc9b9045ef568ea339e246e0
k1yeon <gio13remanente@gmail.com>
2021-04-25 21:45:40 +0800

    deleting post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -113,7 +113,6 @@ class Feed extends Component {
     const formData = new FormData();
     formData.append('title', postData.title);
     formData.append('content', postData.content);
-    console.log(formData.image);
     if (postData.image) formData.append('image', postData.image);
 
     let url = 'http://localhost:8080/feed/post';
@@ -177,7 +176,9 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('URL')
+    fetch(`http://localhost:8080/feed/post/${postId}`, {
+      method: 'DELETE'
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
